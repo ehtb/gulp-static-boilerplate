559afe74be0a964fa5fe9b06b595104b8fac6aef
Edo <i@edo.me>
2015-03-26 13:14:45 +0100

    simplify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var path = require('path');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var plumber = require('gulp-plumber');
 var changed = require('gulp-changed');
 var sass = require('gulp-sass');
@@ -12,54 +11,38 @@ var connect = require('gulp-connect');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var importCss = require('gulp-import-css');
-var webpack = require('webpack');
-var webpackMiddleware = require('webpack-dev-middleware');
-var webpackConfig = require('./webpack.config.js');
+var minifyCSS = require('gulp-minify-css');
+var gulpWebpack = require('gulp-webpack');
 
-var compress = require('compression');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
 
 var neat = require('node-neat');
 var bourbon = require('node-bourbon');
 
-var sourcePath = './src';
-var serverPath = './httpdocs';
-var buildPath = './build';
+var srcPath = './src';
+var servePath = './httpdocs';
 
 var sassIncludePaths = [].concat(
                         neat.includePaths,
                         bourbon.includePaths
                       );
 
-gulp.task('css:compile', function () {
-  gulp.src(sourcePath + '/assets/css/vendor.css')
+gulp.task('css', function () {
+  gulp.src(srcPath + '/assets/css/vendor.css')
     .pipe(plumber())
     .pipe(importCss())
-    .pipe(gulp.dest(serverPath + '/assets/css'));
-});
-
-gulp.task('css:build', function () {
-  gulp.src(sourcePath + '/assets/css/vendor.css')
-    .pipe(plumber())
-    .pipe(importCss())
-    .pipe(gulp.dest(buildPath + '/assets/css'));
-});
-
-gulp.task('sass:compile', function() {
-  gulp.src(sourcePath + '/assets/css/application.scss')
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      errLogToConsole: true,
-      includePaths: sassIncludePaths
+    .pipe(minifyCSS({
+      keepBreaks: true
     }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(serverPath + '/assets/css'))
+    .pipe(gulp.dest(servePath + '/assets/css'))
     .pipe(connect.reload());
 });
 
-gulp.task('sass:build', function() {
-  gulp.src(sourcePath + '/assets/css/application.scss')
+gulp.task('sass', function() {
+  gulp.src(srcPath + '/assets/css/application.scss')
     .pipe(plumber())
+    .pipe(sourcemaps.init())
     .pipe(sass({
       errLogToConsole: true,
       outputStyle: 'compressed',
@@ -69,138 +52,89 @@ gulp.task('sass:build', function() {
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest(buildPath + '/assets/css'));
-});
-
-gulp.task('jade:compile', function() {
-  gulp.src([
-      sourcePath + '/**/*.jade',
-      '!' + sourcePath + '/includes/**/*.*',
-      '!' + sourcePath + '/assets/**/*.*'
-    ])
-    .pipe(plumber())
-    .pipe(jade({
-      locals: require(sourcePath + '/jade')
-    }))
-    .pipe(gulp.dest(serverPath))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(servePath + '/assets/css'))
     .pipe(connect.reload());
 });
 
-gulp.task('jade:build', function() {
+gulp.task('jade', function() {
   gulp.src([
-      sourcePath + '/**/*.jade',
-      '!' + sourcePath + '/includes/**/*.*',
-      '!' + sourcePath + '/assets/**/*.*'
+      srcPath + '/**/*.jade',
+      '!' + srcPath + '/includes/**/*.*',
+      '!' + srcPath + '/assets/**/*.*'
     ])
     .pipe(plumber())
     .pipe(jade({
-      locals: require(sourcePath + '/jade')
+      locals: require(srcPath + '/jade')
     }))
-    .pipe(gulp.dest(buildPath));
+    .pipe(gulp.dest(servePath))
+    .pipe(connect.reload());
 });
 
-gulp.task('images:compile', function() {
-  gulp.src(sourcePath + '/assets/img/**/*')
+gulp.task('images', function() {
+  gulp.src(srcPath + '/assets/img/**/*')
     .pipe(plumber())
-    .pipe(changed(serverPath + '/assets/img'))
+    .pipe(changed(servePath + '/assets/img'))
     .pipe(imagemin({
       svgoPlugins: [{
         cleanupIDs: false
       }]
     }))
-    .pipe(gulp.dest(serverPath + '/assets/img'))
+    .pipe(gulp.dest(servePath + '/assets/img'))
     .pipe(connect.reload());
 });
 
-gulp.task('images:build', function() {
-  gulp.src(sourcePath + '/assets/img/**/*')
-    .pipe(plumber())
-    .pipe(changed(buildPath + '/assets/img'))
-    .pipe(imagemin({
-      svgoPlugins: [{
-        cleanupIDs: false
-      }]
-    }))
-    .pipe(gulp.dest(buildPath + '/assets/img'));
-});
-
-gulp.task('webpack:build', function(callback) {
+gulp.task('webpack', function(callback) {
   var config = Object.create(webpackConfig);
-  config.output.path = path.resolve(buildPath + '/assets/js');
 
-  config.plugins = config.plugins.concat(
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true
-    })
-  );
+  if (process.env.ENV_VARIABLE !== 'production') {
+    config.devtool = 'source-map';
+    config.cache = true;
+    config.debug = true;
+  } else {
+    config.plugins = config.plugins.concat(
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true
+      })
+    );
+  }
 
-  webpack(config, function(err, stats) {
-    if (err) {
-      throw new gutil.PluginError('webpack:build', err);
-    }
-
-    gutil.log('[webpack:build]', stats.toString({
-      colors: true
-    }));
-
-    callback();
-  });
+  gulp.src(srcPath + '/assets/js/**/*')
+    .pipe(plumber())
+    .pipe(gulpWebpack(config))
+    .pipe(gulp.dest(path.resolve(servePath + '/assets/js')))
+    .pipe(connect.reload());
 });
 
 gulp.task('server', function() {
-  var config = Object.create(webpackConfig);
-  config.devtool = 'source-map';
-  config.cache = true;
-  config.debug = true;
-  config.output.path = path.resolve(serverPath + '/assets/js');
-
   connect.server({
-    root: serverPath,
+    root: servePath,
     host: '0.0.0.0',
     port: 8000,
-    livereload: true,
-    middleware: function() {
-      return [
-        compress({
-          level: 9
-        }),
-        webpackMiddleware(webpack(config), {
-          publicPath: config.output.publicPath,
-          stats: {
-            colors: true
-          }
-        })
-      ];
-    }
+    livereload: true
   });
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/assets/css/vendor.js', ['css:compile']);
-  gulp.watch('src/assets/css/**/*', ['sass:compile']);
-  gulp.watch('src/**/*.jade', ['jade:compile']);
-  gulp.watch('src/assets/img/**/*', ['images:compile']);
+  gulp.watch('src/assets/css/vendor.js', ['css']);
+  gulp.watch('src/assets/css/**/*', ['sass']);
+  gulp.watch('src/**/*.jade', ['jade']);
+  gulp.watch('src/assets/img/**/*', ['images']);
+  gulp.watch('src/assets/js/**/*.js', ['webpack']);
 });
 
 gulp.task('compile', [
-  'css:compile',
-  'sass:compile',
-  'jade:compile',
-  'images:compile'
+  'css',
+  'sass',
+  'jade',
+  'images',
+  'webpack'
 ]);
 
 gulp.task('default', ['compile', 'server', 'watch']);
-
-gulp.task('build', [
-  'css:build',
-  'sass:build',
-  'jade:build',
-  'images:build',
-  'webpack:build'
-]);
